39641599e9e3525cee08a30733c67db96781ca9f
Anh <78713930+hna778@users.noreply.github.com>
2026-01-12 13:00:24 -0800

    Update covid19_EDA.sql

--- Covid19/covid19_EDA.sql
+++ Covid19/covid19_EDA.sql
@@ -65,9 +65,6 @@ people_vaccinated = IF(@people_vaccinated = '', NULL, @people_vaccinated)
 SELECT DISTINCT continent, country
 FROM covid_staging;
 
-SELECT DISTINCT country, continent
-FROM covid_staging;
-
 SELECT *
 FROM covid_staging
 WHERE continent = '';
@@ -76,7 +73,9 @@ SELECT DISTINCT country, continent
 FROM covid_staging
 WHERE continent IS NOT NULL;
 
-
+SELECT DISTINCT country, continent
+FROM covid_staging
+WHERE continent = '';
 
 -- By World
 -- 1. Number of Cases, Infection Rate, Case Fatality Rate
@@ -87,10 +86,12 @@ SELECT country AS continent, max(population) AS population
 FROM covid_staging
 WHERE continent = ''
 	AND country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
-GROUP BY country;
+GROUP BY country
+;
 
 SELECT sum(population) AS world_population
 FROM continent_pop;
+
 
 -- Calculate number of Cases, Infection Rate, Case Fatality Rate
 SELECT 	w.world_pop AS world_population,
@@ -121,7 +122,7 @@ FROM (
                 'World excl. China, South Korea, Japan and Singapore')
                 ) 
                 AS world_cases
-	) w;
+	) AS w;
     
 -- By Continents
 -- 2. Number of infected, vaccinated and deaths by Continent
@@ -130,14 +131,83 @@ SELECT 	country AS continent,
 		max(total_cases) AS total_cases,
         max(people_vaccinated) AS total_vaccinations,
         max(total_deaths) AS total_deaths,
-		round(max(total_cases) / max(population), 4) AS infection_rate,
-        round(max(total_deaths) / max(population), 4) AS case_fatality_rate,
-        round(max(people_vaccinated) / max(population), 4) AS vaccination_rate
+		round(max(total_cases) / max(population), 4) * 100 AS infection_rate,
+        round(max(total_deaths) / max(population), 4) * 100 AS mortality_rate,
+        round(max(people_vaccinated) / max(population), 4) * 100 AS vaccination_rate
 FROM covid_staging
 WHERE continent = ''
 	AND country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
 GROUP BY country;
 
+-- First date of Vaccination by Continents
+SELECT 	country AS continent,
+		min(`date`) AS first_vaccination_date
+FROM covid_staging
+WHERE total_vaccinations IS NOT NULL
+	AND country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
+GROUP BY country;
+
+
+-- First date of Vaccination by Continents and Total Vaccination deployed
+SELECT
+    country,
+    `date`,
+    total_vaccinations
+FROM (
+    SELECT
+        country,
+        `date`,
+        total_vaccinations,
+        ROW_NUMBER() OVER (
+            PARTITION BY country
+            ORDER BY `date`
+        ) AS rn
+    FROM covid_staging
+    WHERE total_vaccinations IS NOT NULL
+      AND country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
+) t
+WHERE rn = 1;
+
+-- % Vaccination collected by Year
+SELECT country AS continent,
+		YEAR(`date`) AS `year`,
+        round((max(people_vaccinated)/ max(population)) * 100, 4) AS vaccination_rate
+FROM covid_staging
+WHERE country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
+GROUP BY `year`, country
+ORDER BY continent, `year`;
+
+SELECT
+    country AS continent,
+    ROUND(
+        MAX(CASE WHEN YEAR(`date`) = 2020
+                 THEN people_vaccinated / population END) * 100, 2
+    ) AS `2020`,
+    ROUND(
+        MAX(CASE WHEN YEAR(`date`) = 2021
+                 THEN people_vaccinated / population END) * 100, 2
+    ) AS `2021`,
+    ROUND(
+        MAX(CASE WHEN YEAR(`date`) = 2022
+                 THEN people_vaccinated / population END) * 100, 2
+    ) AS `2022`,
+    ROUND(
+        MAX(CASE WHEN YEAR(`date`) = 2023
+                 THEN people_vaccinated / population END) * 100, 2
+    ) AS `2023`,
+    ROUND(
+        MAX(CASE WHEN YEAR(`date`) = 2024
+                 THEN people_vaccinated / population END) * 100, 2
+    ) AS `2024`
+
+FROM covid_staging
+WHERE country IN ('Africa', 'Europe', 'North America', 'South America', 'Oceania', 'Asia')
+GROUP BY country
+ORDER BY continent;
+
+
+SELECT *
+FROM covid_staging;
 
 -- By Countries
 -- 3. Number of infected, vaccinated and deaths by Country (label)
@@ -159,7 +229,7 @@ WHERE continent IS NOT NULL
 		'Winter Olympics 2022', 'Upper-middle-income countries', 'Wales', 'World excl. China', 'World excl. China and South Korea',
 		'World excl. China, South Korea, Japan and Singapore')
 GROUP BY country, continent
-ORDER BY country;
+ORDER BY total_deaths;
 
 -- 4. Daily Confirmed Deaths 
 SELECT 	continent,
